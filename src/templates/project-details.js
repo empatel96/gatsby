78356bd29867eee643ade6b46a442f7cf8fc4e74
Muhammed Patel <emmpii8@gmail.com>
2021-08-08 20:47:49 +0100

    Update project-details.js

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -2,16 +2,38 @@ import React from 'react';
 import Layout from '../components/Layout';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import styles from '../styles/project-details.module.css';
+import { graphql } from 'gatsby';
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark;
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter;
   return (
     <Layout>
       <div className={styles.details}>
         <h2>title</h2>
         <h3>stack</h3>
-        <div className={styles.featured}>{/* <GatsbyImage image={} alt={} /> */}</div>
-        {/* <div dangerouslySetInnerHTML ={} className={styles.html} /> */}
+        <div className={styles.featured}>
+          <GatsbyImage image={getImage(featuredImg)} alt="Projects" />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: html }} className={styles.html} />
       </div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          }
+        }
+      }
+    }
+  }
+`;
